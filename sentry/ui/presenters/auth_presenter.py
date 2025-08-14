639b388f8b3ed8ea7b69722fcc80db7e4c932fc6
Lucas Fragoso Binder <126175566+LucasBinder115@users.noreply.github.com>
2025-08-14 20:28:05 -0300

    Presenter Criado

---
+++ sentry/ui/presenters/auth_presenter.py
@@ -0,0 +1,14 @@
+# sentry/ui/presenters/auth_presenter.py
+from sentry.auth.credentials import verify_credentials
+from sentry.infra.database.repositories import UserRepository
+
+class AuthPresenter:
+    def __init__(self, view):
+        self.view = view
+        self.repo = UserRepository()
+    
+    def login(self, username: str, password: str) -> bool:
+        user = self.repo.find_by_username(username)
+        if user and verify_credentials(user, password):
+            return True
+        return False
