af6548a07319198840474508997ba9dd9f73ece0
Lucas Binder <lucas@example.com>
2025-10-29 10:36:11 -0400

    Ver 0.2

--- sentry/infra/services/api_adapters/__init__.py
+++
@@ -1,8 +0,0 @@
-# sentry/infra/services/api_adapters/__init__.py
-
-from .denatran import DenatranAPIAdapter # <--- Esta linha agora deve funcionar
-
-class ApiAdapters:
-    @staticmethod
-    def get_safety_adapter():
-        return DenatranAPIAdapter()
